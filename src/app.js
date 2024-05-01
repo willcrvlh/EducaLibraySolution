94a858b82cb76182c2a3d011d3d661e945536b93
Willian Carvalho <wcarvalho91610@gmail.com>
2024-04-30 21:16:00 -0300

    build: importei a controller na app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import booksController from './controller/booksController.js'
 
 const app = express();
 const port = 8080; 
+
+
+booksController(app)
 
 app.listen(port, ()=>{
     console.log(`Serving on port ${port}, http://localhost:${port}`);
